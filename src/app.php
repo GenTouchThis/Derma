1562434b49503415fef211d1012f81bc364b7f87
Carlos García <carlos.agh@gmail.com>
2017-03-31 14:43:15 -0600

    registro
    Registro terminado

--- src/app.php
+++ src/app.php
@@ -5,12 +5,24 @@ use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+	'db.options' => array(
+		'driver'	=> 'pdo_mysql',
+		'dbname'	=> 'dermacos_cursos',
+		'host'		=> 'localhost', // mysql1016.opentransfer.com
+		'user'		=> 'dermacos_r3g1str',
+		'password'	=> 'ap[LZI_Bl07q',
+		'charset'	=> 'utf8'
+		//'unix_socket' => '/var/run/mysqld/mysqld.sock' // '/Applications/MAMP/tmp/mysql/mysql.sock' // /var/run/mysqld/mysqld.sock
+	)
+));
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
--- src/controllers.php
+++ src/controllers.php
@@ -7,72 +7,119 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
+date_default_timezone_set('America/Mexico_City');
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('pages/index.twig.html', array());
+	return $app['twig']->render('pages/index.twig.html', array());
 })->bind('inicio');
 
 $app->get('/laboratorios', function () use ($app) {
-    return $app['twig']->render('pages/laboratorios.twig.html', array());
+	return $app['twig']->render('pages/laboratorios.twig.html', array());
 })->bind('laboratorios');
 
 $app->get('/programa', function () use ($app) {
-    return $app['twig']->render('pages/programa.twig.html', array());
+	return $app['twig']->render('pages/programa.twig.html', array());
 })->bind('programa');
 
 $app->get('/cdmx', function () use ($app) {
-    return $app['twig']->render('pages/cdmx.twig.html', array());
+	return $app['twig']->render('pages/cdmx.twig.html', array());
 })->bind('cdmx');
 
 $app->get('/sede', function () use ($app) {
-    return $app['twig']->render('pages/sede.twig.html', array());
+	return $app['twig']->render('pages/sede.twig.html', array());
 })->bind('sede');
 
 $app->get('/hospedaje', function () use ($app) {
-    return $app['twig']->render('pages/hospedaje.twig.html', array());
+	return $app['twig']->render('pages/hospedaje.twig.html', array());
 })->bind('hospedaje');
 
 $app->get('/exposicion', function () use ($app) {
-    return $app['twig']->render('pages/exposicion.twig.html', array());
+	return $app['twig']->render('pages/exposicion.twig.html', array());
 })->bind('exposicion');
 
 $app->get('/galeria', function () use ($app) {
-    return $app['twig']->render('pages/galeria.twig.html', array());
+	return $app['twig']->render('pages/galeria.twig.html', array());
 })->bind('galeria');
 
 $app->get('/costos', function () use ($app) {
-    return $app['twig']->render('pages/costos.twig.html', array());
+	return $app['twig']->render('pages/costos.twig.html', array());
 })->bind('costos');
 
 $app->get('/ponentes', function () use ($app) {
-    return $app['twig']->render('pages/ponentes.twig.html', array());
+	return $app['twig']->render('pages/ponentes.twig.html', array());
 })->bind('ponentes');
 
-$app->get('/inscripcion', function () use ($app) {
-    return $app['twig']->render('pages/inscripcion.twig.html', array());
+$app->match('/inscripcion', function (Request $request) use ($app) {
+	$db = $app["db"];
+
+	if ($request->getMethod() == "POST") {
+		$json = array(
+			"status" => false,
+			"mensaje" => "Hubo un error al procesar su registro, por favor, intentelo de nuevo."
+		);
+		$folioRegistro = time();
+		$fechaRegistro = new \DateTime('now');
+
+		$result = $db->insert("registros", array(
+				"folioRegistro" => $folioRegistro,
+				"nombre" => $request->request->get("nombre"),
+				"paterno" => $request->request->get("paterno"),
+				"materno" => $request->request->get("materno"),
+				"email" => $request->request->get("email"),
+				"requiereFactura" => $request->request->get("requiereFactura"),
+				"razonSocial" => $request->request->get("razonSocial"),
+				"rfc" => $request->request->get("rfc"),
+				"direccion" => $request->request->get("direccion"),
+				"numExt" => $request->request->get("numExt"),
+				"numInt" => $request->request->get("numInt"),
+				"colonia" => $request->request->get("colonia"),
+				"delOMun" => $request->request->get("delOMun"),
+				"estado" => $request->request->get("estado"),
+				"pais" => $request->request->get("pais"),
+				"emailFacturacion" => $request->request->get("emailFacturacion"),
+				"fechaRegistro" => $fechaRegistro->format("Y-m-d H:i:s")
+			)
+		);
+
+		if ($result) {
+			$json["status"] = true;
+			$json["folioRegistro"] = $folioRegistro;
+			$json["email"] = $request->request->get("email");
+		}
+
+		return $app->json($json);
+	} else {
+		$sql = "SELECT * FROM paises WHERE 1 ORDER BY pais_nombreEs ASC";
+		$paises = $db->fetchAll($sql);
+	}
+
+	return $app['twig']->render('pages/inscripcion.twig.html', array(
+			"paises" => $paises
+		)
+	);
 })->bind('inscripcion');
 
 $app->get('/contacto', function () use ($app) {
-    return $app['twig']->render('pages/contacto.twig.html', array());
+	return $app['twig']->render('pages/contacto.twig.html', array());
 })->bind('contacto');
 
 /*active items */
 $app->before(function ($request) use ($app) {
-    $app['twig']->addGlobal('active', $request->get("_route"));
+	$app['twig']->addGlobal('active', $request->get("_route"));
 });
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
-    if ($app['debug']) {
-        return;
-    }
+	if ($app['debug']) {
+		return;
+	}
 
-    // 404.html, or 40x.html, or 4xx.html, or error.html
-    $templates = array(
-        'errors/'.$code.'.twig.html',
-        'errors/'.substr($code, 0, 2).'x.twig.html',
-        'errors/'.substr($code, 0, 1).'xx.twig.html',
-        'errors/default.twig.html',
-    );
+	// 404.html, or 40x.html, or 4xx.html, or error.html
+	$templates = array(
+		'errors/'.$code.'.twig.html',
+		'errors/'.substr($code, 0, 2).'x.twig.html',
+		'errors/'.substr($code, 0, 1).'xx.twig.html',
+		'errors/default.twig.html'
+	);
 
-    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+	return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
 });
